6c6402d91cff0c3d490d383da7f682383e0b418f
Samuel Squire <sam@samsquire.com>
2023-09-27 16:48:56 +0100

    Sam zero clause BSD licence

--- barrier.c
+++ barrier.c
@@ -1,3 +1,11 @@
+/*
+Copyright (C) 2023 by Samuel Michael Squire sam@samsquire.com
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*/
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -121,7 +129,7 @@ int barriered_reset(volatile struct BarrierTask *data) {
 }
 
 int main() {
-  int thread_count = 12;
+  int thread_count = 5;
   int timer_count = 1;
   struct KernelThread *thread_data = calloc(thread_count + 1, sizeof(struct KernelThread)); 
 
