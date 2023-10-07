631756a0505a38a1bc8bd30f16d9da7387d19beb
Samuel Squire <sam@samsquire.com>
2023-10-07 12:38:18 +0100

    Sam two writes to the same location

--- barrier-runtime-safe.c
+++ barrier-runtime-safe.c
@@ -31,15 +31,15 @@ NonBlockingBarrierSynchronizationPreempt.java
 struct BarrierTask {
   int task_index;
   int rerunnable;
-  volatile int arrived; 
+  _Atomic int arrived; 
   long n; 
   int (*run)(volatile struct BarrierTask*);
   struct KernelThread *thread;
-  volatile int thread_index;
+  int thread_index;
   int thread_count;
   volatile int available;
   int task_count;
-  volatile int scheduled;
+  _Atomic int scheduled;
 };
 
 struct KernelThread {
@@ -51,13 +51,12 @@ struct KernelThread {
   int total_thread_count;
   volatile struct BarrierTask *tasks;
   int task_count;
-  volatile int running;
+  _Atomic volatile int running;
   struct ProtectedState *protected_state;
 };
 
 struct ProtectedState {
-  long protected;
-  int modcount;
+  int protected;
 };
 
 void* io_thread(void *arg) {
@@ -75,12 +74,11 @@ void* barriered_thread(void *arg) {
   // printf("In barrier task %d\n", data->thread_index);
   int t = 0;
   while (data->running == 1) {
-    if (t >= data->task_count) {
+    if (t >= data->task_count - 1) {
       t = 0;
     }
-    // printf("%d reporting %d %d\n", data->thread_index, t, data->task_count);
     for (; t < data->task_count; t++) {
-      // printf("%d %d\n", data->thread_index, t);
+      // printf("%d\n", t);
       if (data->tasks[t].available == 1) {
         int previous = t;
         if (t > 0) {
@@ -98,15 +96,13 @@ void* barriered_thread(void *arg) {
         if (arrived == 0 || arrived == data->thread_count) {
           // we can run this task
 
-          asm volatile ("mfence" ::: "memory");
           data->tasks[t].available = 0;
-
-          // printf("In thread %d %d\n", data->thread_index, t);
+          asm volatile ("mfence" ::: "memory");
           data->tasks[t].run(&data->threads[data->thread_index].tasks[t]);
           data->tasks[t].arrived++;
-          // break;
+          break;
         } else {
-          // printf("%d %d %d\n", data->thread_index, t, arrived);
+          // printf("%d %d\n", t, arrived);
           break;
         }   
       } else {
@@ -173,26 +169,14 @@ void* timer_thread(void *arg) {
   printf("Timer thread stopping\n");
   return 0;
 }
-int do_protected_write(volatile struct BarrierTask *data, struct ProtectedState *protected) {
-  if (data->task_index == data->thread_index) {
-    data->n++;
-    int modcount = ++protected->modcount;
-    // printf("Protected %d %d\n", data->task_index, data->thread_index);
-    protected->protected++;
-    if (modcount != protected->modcount) {
-      printf("Race condition!\n");
-    } 
-  }
-  
-}
-
 int barriered_work(volatile struct BarrierTask *data) {
   // printf("In barrier work task %d %d\n", data->thread_index, data->task_index);
-  // printf("%d Arrived at task %d\n", data->thread_index, data->task_index);
+  printf("%d Arrived at task %d\n", data->thread_index, data->task_index);
   volatile long *n = &data->n;
   while (data->scheduled == 1) {
-    do_protected_write(data, data->thread->protected_state);
+    data->n++;
   }
+  
   // data->thread->protected_state->protected++;
   return 0;
 }
@@ -208,19 +192,17 @@ int barriered_steal(volatile struct BarrierTask *data) {
 }
 int barriered_reset(volatile struct BarrierTask *data) {
   // printf("In barrier reset task %d %d\n", data->thread_index, data->task_index);
-    for (int x = 0 ; x < data->task_count ; x++) {
-      // printf("Resetting %d %d\n", n, x);
-      data->thread->threads[data->thread_index].tasks[x].arrived++; 
-      // data->thread->tasks[x].arrived++; 
-      
-      data->thread->tasks[x].available = 1; 
+  for (int x = 0 ; x < data->task_count ; x++) {
+    // printf("Resetting %d\n", x);
+    data->thread->tasks[x].arrived++; 
+    data->thread->tasks[x].available = 1; 
   }
   asm volatile ("mfence" ::: "memory");
   return 0;
 }
 
 int main() {
-  int thread_count = 10;
+  int thread_count = 11;
   int timer_count = 1;
   int io_threads = 1;
   int total_threads = thread_count + timer_count + io_threads;
@@ -244,9 +226,6 @@ int main() {
       thread_data[x].tasks = barriers;
 
       for (int y = 0 ; y < barrier_count ; y++) {
-        if (x == y) {
-          printf("%d == %d\n", x, y);
-        }
         thread_data[x].tasks[y].thread_index = x;
         thread_data[x].tasks[y].thread = &thread_data[x]; 
         thread_data[x].tasks[y].available = 1;
@@ -328,7 +307,6 @@ int main() {
     }
   }
   printf("Total Requests %ld\n", total);
-  printf("Total Protected %ld\n", protected_state->protected);
   printf("Total Requests per second %ld\n", total / DURATION);
   return 0;
 
