2340d82bc0d2c11a3e371062964ad925783b4967
Samuel Squire <sam@samsquire.com>
2023-10-11 10:30:12 +0100

    Sam balanced thread ingests 90 million and 237 intra thread requests

--- barrier-runtime.c
+++ barrier-runtime.c
@@ -513,7 +513,7 @@ int main() {
   int timer_count = 1;
   int io_threads = 1;
   int external_threads = 3;
-  int buffer_size = 999999;
+  int buffer_size = 9999999;
   int total_threads = thread_count + timer_count + io_threads + external_threads;
   struct ProtectedState *protected_state = calloc(1, sizeof(struct ProtectedState));
   struct KernelThread *thread_data = calloc(total_threads, sizeof(struct KernelThread)); 
@@ -561,7 +561,7 @@ int main() {
         struct Mailbox *mailboxes = calloc(thread_count, sizeof(struct Mailbox));
         thread_data[x].tasks[y].mailboxes = mailboxes;
         // long messages_limit = 20;/*9999999;*/
-        long messages_limit = 99999;
+        long messages_limit = 999999;
         for (int b = 0 ; b < thread_count ; b++) {
           struct Message **messages = calloc(messages_limit, sizeof(struct Message*));
           struct Message **messages2 = calloc(messages_limit, sizeof(struct Message*));
