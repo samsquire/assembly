a6b4cc2dd10f723065bec26dddfd64868258d3b1
Samuel Squire <sam@samsquire.com>
2023-12-12 06:30:25 +0000

    Sam specgenerator

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd c:\Users\samue\Downloads\TLAToolbox-1.8.0-win32.win32.x86_64\toolbox
+java -cp tla2tools.jar tlc2.TLC c:\Users\samue\OneDrive\assembly\ringbuffer-tla\ringbuffer_1.toolbox\ringbuffer\MC.tla -dump dot spec.dot
