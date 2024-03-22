f90f9e391c22e76bca9034ab0b8b164b87fc422a
Samuel Squire <sam@samsquire.com>
2024-03-22 17:10:51 +0000

    Out of order execution

--- threadsafe-workqueue.c
+++ threadsafe-workqueue.c
@@ -1,6 +1,6 @@
 #define _GNU_SOURCE
- #include <err.h>
- #include <errno.h>
+#include <err.h>
+#include <errno.h>
 #include <pthread.h>
 #include <stdio.h> 
 #include <time.h> 
@@ -8,15 +8,16 @@
 #include <string.h>
 struct Work {
   int taskindex;
-  int available;
+  int available __attribute__((aligned (128)));
 };
+// __attribute__((aligned (128)))
 struct Data {
   struct Data *main;
   struct Data *threads;
-  long freq;
+  long freq ;
   int taskindex; 
-  int workindex;
-  int wantindex;
+  int workindex __attribute__((aligned (128)));
+  int wantindex __attribute__((aligned (128)));
   int running;
   int worksize;
   int threadindex;
@@ -24,6 +25,9 @@ struct Data {
   struct Work *works;
   cpu_set_t *cpu_set;
   int failcounter;
+  int loglevel;
+  int *primes;
+  int buckets;
 };
 
 /*
@@ -44,8 +48,8 @@ void * work(void * arg) {
   char * output = calloc(100, sizeof(char));
   
   while (data->running == 1) {
-    memset(output, 0, 100);
-   if (data->threadindex != 0 && data->main->workindex >= data->worksize) { continue; }
+  memset(output, 0, 100);
+   
     //if (data->threadindex == 0 && data->main->workindex >= data->worksize) {
     /*if (data->main->workindex >= data->worksize){
         data->main->workindex = 0;
@@ -57,15 +61,46 @@ void * work(void * arg) {
       asm volatile ("" ::: "memory");
       int allunavailable = 1;
       int available = 1;
-    int target = (data->main->workindex);
-   for (int x = target ; x < data->worksize ; x++ ) {
+      int target = (data->threadindex * data->buckets);
+    
+    int bucketlim = (data->threadindex + 1) * data->buckets;
+    // printf("target: %d %d\n", target, bucketlim);
+    int stride = 1;
+    int found = 0;
+    int mine = 1;
+   for (int x = data->workindex ; x < bucketlim ; x+= stride ) {
+       //printf("%d\n", x); 
+    if (data->main->works[x].available == 1 ) {
+        target = x;
+        found = 1;
+        break;
+    } else {
+        
+      
+    }
+   }
+    
+  if (found == 0) {
+    //printf("expired\n");
+    mine = 0;
+    target = 0;
+    int stride = 1;
+    
+   for (int x = target ; x < data->worksize ; x+= stride ) {
         
     if (data->main->works[x].available == 1 ) {
         target = x;
+        
         break;
-    }
-   } 
+    } else {
+        
       
+    }
+   }
+  }
+    data->workindex = target;
+    if (mine == 0) {
+      data->workindex = target;
       data->threads[data->threadindex].wantindex = target;
       for (int x = 0; x < data->threadsize ; x++ ) {
         if (x == data->threadindex) {
@@ -79,30 +114,32 @@ void * work(void * arg) {
           break;
         }
       }
-      
+    }
       if (available == 1 && data->main->works[target].available == 1) {
         data->freq++;
+        if (data->loglevel == 1) {
         if (data->threadindex == 0) {
           ;
         snprintf(output, 100, "queue owner [%d]: processing queue item %d", data->threadindex, target);
         printf("%s\n", output); 
 
         } else {
+          
 snprintf(output, 100, "queue other [%d]: stealing queue item %d", data->threadindex, target);
          printf("%s\n", output);
           
           
-        }
+        } }
         data->main->works[target].available = 0;
         data->threads[data->threadindex].wantindex = -1;
-        data->main->workindex = (target + 1);
-        if (data->main->workindex >= data->worksize){
-        data->main->workindex = 0;
-        printf("work epoch end\n");
-        for (int x = 0 ; x < data->worksize; x++) {
+        //data->main->workindex = (target + 1);
+        if (found == 0) {
+        data->workindex = (data->threadindex * data->buckets);
+        if (data->loglevel == 1) { printf("work epoch end\n"); }
+        for (int x = data->buckets * data->threadindex ; x < bucketlim; x++) {
           data->main->works[x].available = 1;
         }
-          printf("reset\n");
+          
         }
         
         
@@ -133,8 +170,12 @@ snprintf(output, 100, "queue other [%d]: stealing queue item %d", data->threadin
   }
 
 int main(int argc, char **argv) {
-  int worksize = 10;
-  int threadsize = 6;
+  int debug = 0;
+  int worksize = 10000000;
+  
+  int primes[] = {3, 7, 13, 19, 23, 29, 31, 37};
+  int threadsize = 7;
+  int buckets = worksize / threadsize;
   printf("Starting %d workers\n", threadsize);
   pthread_t *thread = calloc(threadsize, sizeof(pthread_t));
   pthread_attr_t *attr = calloc(threadsize, sizeof(pthread_attr_t));
@@ -150,12 +191,14 @@ int main(int argc, char **argv) {
   data[0].works = works;
   for (int x = 0; x < threadsize ; x++) {
     data[x].cpu_set = calloc(1, sizeof(cpu_set_t));
-    CPU_SET(cpu += 2, data[x].cpu_set);
+    CPU_SET(cpu += 1, data[x].cpu_set);
     printf("assigning thread %d to cpu %d\n", x, cpu);
+    data[x].loglevel = debug;
     data[x].running = 1;
     data[x].threadindex = x;
     data[x].worksize = worksize;
     data[x].threadsize = threadsize;
+    data[x].buckets = buckets;
     data[x].main = &data[0];
     data[x].threads = data;
     data[x].wantindex = -1;
