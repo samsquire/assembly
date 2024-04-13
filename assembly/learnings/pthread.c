0b58d8ecea78c6e6db77bd1221d1f4800e6523fa
Samuel Squire <sam@samsquire.com>
2024-04-13 17:09:49 +0000

    Need to Initialize memory with zeros

--- assembly/learnings/pthread.c
+++ assembly/learnings/pthread.c
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <unistd.h>
 
-static 
 void *func(void *data) {
   printf("Hi from thread\n");
   return 0;
@@ -18,21 +17,22 @@ struct data {
 
 int main() {
   ssize_t             stack_size;
-  pthread_attr_t      attr;
-  pthread_attr_t      attr2;
-  pthread_t thread;
-  //pthread_t thread2;
+  pthread_attr_t      attr = {0};
+  pthread_attr_t      attr2 = {0};
+  pthread_t thread = {0};
+  pthread_t thread2 = {0};
   struct data data; 
 
   int s = pthread_create(&thread, &attr,
 
-                            &func, &data); 
+                            func, &data); 
   printf("thread 1 created %ld %ld\n", sizeof(pthread_t), sizeof(pthread_attr_t));
-  // int d = pthread_create(&thread2, &attr2,
-  //                          &func, &data); 
-  // printf("thread 2 created\n");
+  
+   int d = pthread_create(&thread2, &attr2,
+                           func, &data); 
+  printf("thread 2 created\n");
   void *result;
   pthread_join(thread, &result);
-  // pthread_join(thread2, &result);
+   pthread_join(thread2, &result);
 }
 
